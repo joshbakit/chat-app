3edda91b87dd8dc37c87ef3c58ae4c8e0fa6f5a7
Joshua <benasjoshuac@gmail.com>
2024-12-29 20:43:54 +0800

    updates

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,7 +4,9 @@ import {
   login,
   logout,
   updateProfile,
+  checkAuth,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -12,6 +14,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.put("update-profile", updateProfile)
+router.put("update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
